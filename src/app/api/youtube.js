f556bb84382501e640f4d68f1fa484a5fa5749dd
Arpit Pathak <110691809+Coder1709@users.noreply.github.com>
2023-11-02 22:47:06 +0530

    updated the api

--- src/app/api/youtube.js
+++ src/app/api/youtube.js
@@ -1,22 +1,43 @@
-export default async(req , res) => {
-    var key = process.env.YOUTUBE_API_KEY;
+// export default async(req , res) => {
+//     var key = process.env.YOUTUBE_API_KEY;
 
-    const response = await fetch ('https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=2&q=hindi%20music%20&key=AIzaSyBfhQInmH1wqUmBGpQEKsBfPRDYQxBtajA')
-    const json = await response.json()
-    const videoData = json.items.map(item => ({
-        videoId: item.id.videoId,
-        title: item.snippet.title,
-        description: item.snippet.description,
-      }));
-
-    return res.status(200).json({
-        videoId,title,description
-    })
-    
-      
-      
-      
+import axios from "axios";
 
 
 
-}
+export default async (req, res) => {
+    try {
+        var key = process.env.YOUTUBE_API_KEY;
+
+        const apiUrl = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=2&q=hindi%20music%20&key=AIzaSyBfhQInmH1wqUmBGpQEKsBfPRDYQxBtajA';
+
+        axios.get(apiUrl).then(res => {
+            console.log(res)
+        }).catch(err => {
+            console.log(err)
+        })
+
+        const response = await fetch(apiUrl, {
+            method: 'GET',
+            // headers: {
+            //   'Content-Type': 'application/json',
+            // },
+        });
+
+        if (!response.items) {
+            throw new Error(`Request failed with status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        const videoData = json.items.map((item) => ({
+            videoId: item.id.videoId,
+            title: item.snippet.title,
+            description: item.snippet.description,
+        }));
+
+        return res.status(200).json(videoData);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
--- src/app/component/Songs.js
+++ src/app/component/Songs.js
@@ -1,47 +1,67 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useAuth } from '../auth/auth';
 import useSWR from 'swr'
-import youtube from '../api/youtube';
-import fetcher from '../lib/fetcher';
+import axios from 'axios';
+// import youtube from '../api/youtube';
+// import fetcher from '../lib/fetcher';
 
 
 
 
 const Songs = () => {
-    const {score ,setScore  } = useAuth();
+    const { score, setScore } = useAuth();
+    const [ytSongs, setYtSongs] = useState(null)
 
 
     const incrementScore = () => {
         setScore(score + 1);
-        
+
     };
-    
+
+    useEffect(() => {
+        const apiUrl = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=2&q=trending%20hindi%20songs%20&key=AIzaSyBfhQInmH1wqUmBGpQEKsBfPRDYQxBtajA';
+        fetch(apiUrl).then(res => res.json()).then(res => { console.log(res) }).catch(console.log)
+
+        // axios.get(apiUrl).then(res => {
+        //     console.log(res)
+        //     setYtSongs(res.data.items);
+        // }).catch(err => {
+        //     console.log(err)
+        // })
+    }, [])
 
 
-    const { data , error} = useSWR('/api/youtube.js',fetcher);
 
-    if(error){
-        console.log(error);
-    }
-    if(!data){
-        return (
-            <div>---</div>
-        )
-    }
-    const videoId = data?.videoId;
-    const title = data.title;
-    const description = data?.description
+    // const { data, error } = useSWR('/api/youtube.js', fetcher);
+
+    // if (error) {
+    //     console.log(error);
+    // }
+    // if (!data) {
+    //     return (
+    //         <div>---</div>
+    //     )
+    // }
+    // const videoId = data?.videoId;
+    // const title = data.title;
+    // const description = data?.description
     return (
         <div className='p-3.5  '>
-        <h1 className="text-center text-shadow-lg text-4xl rounded-2xl px-3.5 py-3.5 font-bold text-red-500 ">Your Score is {score}</h1>
+            <h1 className="text-center text-shadow-lg text-4xl rounded-2xl px-3.5 py-3.5 font-bold text-red-500 ">Your Score is {score}</h1>
 
-        
+
+
             <div className="grid grid-cols-2 gap-4 p-15">
-                <div className="w-1/2 h-120 p-2 m-3 b-3 items-center bg-green-100">
-                <button onClick={incrementScore}>Content in Div 1{title}</button></div>
-                <div className="w-1/2 h-120 p-2 m-3  b-3 items-center bg-green-100">
-                <button onClick={incrementScore}>Content in Div 2</button>
-                </div>
+                {Array.isArray(ytSongs) && ytSongs.map(e => {
+                    return <div className="w-1/2 h-120 p-2 m-3 b-3 items-center bg-green-100">
+                        <iframe width="800" height="600"
+                            src={`https://www.youtube.com/embed/${e.id.videoId}?autoplay=1`}>
+                        </iframe>
+
+                        <button onClick={incrementScore}>{e.id.videoId}</button>
+                    </div>
+                })}
+
             </div>
         </div>
 
